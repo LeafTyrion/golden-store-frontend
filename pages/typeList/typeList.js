577adc371bd0d14fcf8eb76275e9c402f7685a7a
Tyrion <tyrion@TyriondeMacBook-Pro.local>
2019-09-15 15:20:05 +0800

    myGoldenStore

---
+++ pages/typeList/typeList.js
@@ -0,0 +1,105 @@
+// pages/typeList/typeList.js
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    itemArray: [
+      {
+        "id":1,
+        "itemText": '11月20日话剧《风声》'
+      },
+      {
+        "id": 1,
+        "itemText": '11月20日话剧《原野》'
+      },
+      {
+        "id": 1,
+        "itemText": '11月28日“夜店”演唱会'
+      },
+      {
+        "itemText": '11月20日话剧《风声》'
+      },
+      {
+        "itemText": '11月20日话剧《原野》'
+      },
+      {
+        "itemText": '11月28日“夜店”演唱会'
+      },
+      {
+        "itemText": '11月20日话剧《风声》'
+      },
+      {
+        "itemText": '11月20日话剧《风声》'
+      },
+      {
+        "itemText": '11月20日话剧《原野》'
+      },
+      {
+        "itemText": '11月28日“夜店”演唱会'
+      },
+      {
+        "itemText": '11月20日话剧《风声》'
+      }
+    ],
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
