4949a581f6641049c793ce297f4a24fc38924e97
Tyrion <tyrion@TyriondeMacBook-Pro.local>
2019-09-15 22:45:36 +0800

    cart

--- pages/shopCar/shopCar.js
+++ pages/shopCar/shopCar.js
@@ -5,14 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    carts: [], //数据 
+    iscart: true,
+    hidden: null,
+    // isAllSelect: true,
+    totalMoney: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 获取产品展示页保存的缓存数据（购物车的缓存数组，没有数据，则赋予一个空数组）  
 
+    var arr = wx.getStorageSync('cart') || [];
+    console.info("缓存数据：" + arr);
+    // 有数据的话，就遍历数据，计算总金额 和 总数量  
+    if (arr.length > 0) {
+      // 更新数据  
+      this.setData({
+        carts: arr,
+        iscart: true,
+        hidden: false,
+
+      });
+      console.info("缓存数据：" + this.data.carts);
+    } else {
+      this.setData({
+        iscart: false,
+        hidden: true,
+      });
+    }
+    var totalMoney = 0
+    for (var i = 0; i < this.data.carts.length; i++) {
+
+      totalMoney += (this.data.carts[i].price * this.data.carts[i].count);
+    }
+    this.setData({
+      totalMoney: totalMoney,
+    })
+    wx.setStorageSync("totalMoney", totalMoney)
   },
 
   /**
