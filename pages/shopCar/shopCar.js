81bc2114cd61e300d0715982a3c406946084c098
Jack <Jack@qq.com>
2019-10-14 20:57:48 +0800

    add proList

--- pages/shopCar/shopCar.js
+++ pages/shopCar/shopCar.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    cartGoods: [],
   },
 
   /**
@@ -62,5 +62,10 @@ Page({
    */
   onShareAppMessage: function () {
     
+  },
+  toIndex: function(){
+    wx.switchTab({
+      url: '/pages/indexPage/indexPage',
+    })
   }
 })
